715a3a408c66da9e4a6aa180208cbb00e18905b3
Mikel Aramendia <mikel.aramendia.lacalle@gmail.com>
2024-05-11 06:50:51 +0200

    create repo

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // Directorio para almacenar archivos cargados temporalmente
+
 
 var app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const uploadedFile = req.file;
+
+  if (!uploadedFile) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  const fileInfo = {
+    name: uploadedFile.name,
+    type: uploadedFile.mimetype,
+    size: uploadedFile.size
+  };
+
+  res.json(fileInfo);
+});
+
 
 
 
